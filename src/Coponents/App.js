86783909595147ee14811a8e3ff4e2a88f7cd56f
Karan <karanchandekar431@gmail.com>
2023-01-17 00:20:49 +0530

    created iframes

--- src/Coponents/App.js
+++ src/Coponents/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import Editor from "./Editor";
+
 function App() {
-  return "Hello World!";
+  return (
+    <React.Fragment>
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
